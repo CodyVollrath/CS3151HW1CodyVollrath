a247223389fbbc5447efa6b66f7ce20de679d8ac
CodyVollrath <950doenut.buff@gmail.com>
2021-01-13 18:42:24 -0500

    Added tests to test get

--- src/edu/westga/dsdm/testdoublylinkedlist/TestGet.java
+++ src/edu/westga/dsdm/testdoublylinkedlist/TestGet.java
@@ -1,19 +1,43 @@
 package edu.westga.dsdm.testdoublylinkedlist;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestGet {
+import edu.westga.dsdm.model.DoublyLinkedList;
 
+public class TestGet {
+
+	private DoublyLinkedList<Integer> list;
+	
 	@BeforeEach
-	void setUp() throws Exception {
+	public void setUp() throws Exception {
+		this.list = new DoublyLinkedList<Integer>();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testGetFromEmptyList() {
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			this.list.get(0);
+		});
+	}
+	
+	@Test
+	public void testGetFromSingleItemList() {
+		this.list.add(0, 1);
+		assertEquals(1, this.list.get(0));
+	}
+	
+	@Test
+	public void testGetFromMultiItemList() {
+		this.list.add(0, 1);
+		this.list.add(1, 2);
+		this.list.add(2, 3);
+		assertEquals(2, this.list.get(1));
 	}
 
 }
