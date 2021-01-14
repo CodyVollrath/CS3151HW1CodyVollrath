ff920ed41eee3a9768b484ebc088151dbf9b86fb
CodyVollrath <950doenut.buff@gmail.com>
2021-01-13 19:09:18 -0500

    Added tests to get Head

--- src/edu/westga/dsdm/testdoublylinkedlist/TestGetHead.java
+++ src/edu/westga/dsdm/testdoublylinkedlist/TestGetHead.java
@@ -1,19 +1,36 @@
 package edu.westga.dsdm.testdoublylinkedlist;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import edu.westga.dsdm.model.DoublyLinkedList;
+
 class TestGetHead {
 
+	private DoublyLinkedList<Integer> list;
 	@BeforeEach
-	void setUp() throws Exception {
+	public void setUp() throws Exception {
+		this.list = new DoublyLinkedList<Integer>();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testGetEmptyHead() {
+		assertEquals(null, this.list.getHead());
+	}
+	
+	@Test
+	public void testGetSingleHead() {
+		this.list.add(0, 1);
+		assertEquals(1, this.list.getHead());
+	}
+	
+	@Test
+	public void testGetMultiItemHead() {
+		this.list.add(0, 1);
+		this.list.add(1, 2);
+		this.list.add(2, 3);
+		assertEquals(1, this.list.getHead());
 	}
 
 }
